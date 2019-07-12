cce441425947959e0f39db1e918be57c743ac12d
tnt2017 <tnt2011@bk.ru>
2019-07-12 17:06:18 +0700

    Support MYSQL 8.0 12-07-2019

---
+++ Form1.Designer.cs
@@ -0,0 +1,402 @@
+﻿namespace cons_test_maker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox_sql = new System.Windows.Forms.TextBox();
+            this.textBox_batnik = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(14, 8);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1220, 549);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox_sql);
+            this.groupBox1.Controls.Add(this.textBox_batnik);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.textBox10);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.textBox9);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.textBox7);
+            this.groupBox1.Controls.Add(this.textBox8);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(14, 561);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(1220, 185);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+             // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(247, 155);
+            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(66, 13);
+            this.label10.TabIndex = 38;
+            this.label10.Text = "Дата конца";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(329, 157);
+            this.textBox10.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(153, 20);
+            this.textBox10.TabIndex = 37;
+            this.textBox10.Text = "contact";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(19, 157);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(71, 13);
+            this.label9.TabIndex = 36;
+            this.label9.Text = "Дата начала";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(97, 155);
+            this.textBox9.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(129, 20);
+            this.textBox9.TabIndex = 35;
+            this.textBox9.Text = "contact";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(258, 122);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 34;
+            this.label5.Text = "Email";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(258, 89);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 33;
+            this.label6.Text = "Телефон";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(258, 57);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.TabIndex = 32;
+            this.label7.Text = "Адрес";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(258, 21);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 13);
+            this.label8.TabIndex = 31;
+            this.label8.Text = "Город";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(329, 23);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(153, 20);
+            this.textBox5.TabIndex = 30;
+            this.textBox5.Text = "city";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(329, 55);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(153, 20);
+            this.textBox6.TabIndex = 29;
+            this.textBox6.Text = "adres";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(329, 85);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(153, 20);
+            this.textBox7.TabIndex = 28;
+            this.textBox7.Text = "phone";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(329, 118);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(151, 20);
+            this.textBox8.TabIndex = 27;
+            this.textBox8.Text = "email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 123);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Контакт";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 91);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Имя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 59);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Пароль";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Логин";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(97, 122);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(129, 20);
+            this.textBox4.TabIndex = 22;
+            this.textBox4.Text = "contact";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(96, 91);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(129, 20);
+            this.textBox3.TabIndex = 21;
+            this.textBox3.Text = "name";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(96, 59);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(129, 20);
+            this.textBox2.TabIndex = 20;
+            this.textBox2.Text = "554668";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 23);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 20);
+            this.textBox1.TabIndex = 19;
+            this.textBox1.Text = "test0511";
+            // 
+            // textBox_sql
+            // 
+            this.textBox_sql.Location = new System.Drawing.Point(740, 20);
+            this.textBox_sql.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_sql.Multiline = true;
+            this.textBox_sql.Name = "textBox_sql";
+            this.textBox_sql.Size = new System.Drawing.Size(466, 63);
+            this.textBox_sql.TabIndex = 40;
+             // 
+            // textBox_batnik
+            // 
+            this.textBox_batnik.Location = new System.Drawing.Point(740, 88);
+            this.textBox_batnik.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_batnik.Multiline = true;
+            this.textBox_batnik.Name = "textBox_batnik";
+            this.textBox_batnik.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_batnik.Size = new System.Drawing.Size(466, 95);
+            this.textBox_batnik.TabIndex = 39;
+             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(517, 118);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(187, 38);
+            this.button3.TabIndex = 43;
+            this.button3.Text = "Создать bat для всех юзеров";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_2);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(516, 71);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(189, 39);
+            this.button2.TabIndex = 42;
+            this.button2.Text = "Создать доступ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(516, 28);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 39);
+            this.button1.TabIndex = 41;
+            this.button1.Text = "Обновить список";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1263, 754);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Создавалка доступов К+ (12-07-2019)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox_sql;
+        private System.Windows.Forms.TextBox textBox_batnik;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.IO;
+
+namespace cons_test_maker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        mysql_db db = new mysql_db();
+
+
+        public class mysql_db
+        {
+            MySqlConnection con;
+            private string ConnectionString;
+
+
+            public mysql_db()
+            {
+                ConnectionString = "server=127.0.0.1;user=root;database=db_tntnets;password=++++++;charset=utf8; Allow Zero Datetime=true;";
+                Connect();
+            }
+
+            public void Connect()
+            {
+                try
+                {
+                    con = new MySqlConnection();
+
+                    //ConnectionString = "server=\"" + ConfigurationManager.AppSettings["mysql_host"] + "\";user=" +
+                    //                    ConfigurationManager.AppSettings["mysql_login"] + ";database=" + ConfigurationManager.AppSettings["mysql_db"] + ";password=" + ConfigurationManager.AppSettings["mysql_pass"] + ";charset=utf8; Allow Zero Datetime=true;";
+
+                    con.ConnectionString = ConnectionString;
+                    MySqlConnectionStringBuilder mysqlCSB = new MySqlConnectionStringBuilder();
+                    mysqlCSB.ConnectionString = ConnectionString;
+                    con.Open();
+                }
+                catch (Exception exception)
+                {
+                    write_log("Не удалось подключиться к mysql серверу");
+                }
+            }
+
+            void write_log(string s)
+            {
+
+            }
+
+            public void SqlQuery(string sql, string message)
+            {
+                using (MySqlConnection con = new MySqlConnection(ConnectionString))
+                {
+                    try
+                    {
+                        MySqlCommand cmd = new MySqlCommand(sql, con);
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+
+                        //MessageBox.Show(message);
+                    }
+
+                    catch (Exception ex)
+                    {
+                        write_log(ex.Message);
+                    }
+                }
+            }
+
+            public string SqlQueryWithResult(string sql)
+            {
+                DataTable dt = Get_DataTable(sql);
+                if (dt == null)
+                    return "0";
+                else
+                {
+                    if (dt.Rows.Count != 0)
+                        return dt.Rows[0][0].ToString();
+                    else
+                        return "0";
+                }
+            }
+
+            public DataTable Get_DataTable(string queryString)
+            {
+                DataTable dt = new DataTable();
+                MySqlCommand com = new MySqlCommand(queryString, con);
+
+                try
+                {
+                    using (MySqlDataReader dr = com.ExecuteReader())
+                    {
+                        if (dr.HasRows)
+                        {
+                            dt.Load(dr);
+                        }
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                return dt;
+            }
+
+            public DataTable GetPhones(string id, string num, string base_name)
+            {
+                string q = @"SELECT phone from " + base_name + " WHERE state='0' LIMIT 0, " + num.ToString();
+                return Get_DataTable(q);
+            }
+
+
+            public string[] GetPhones(int num, string base_name)
+            {
+                DataTable dt = GetPhones("", num.ToString(), base_name);
+
+                string[] readText = new string[dt.Rows.Count];
+
+                int i = 0;
+                foreach (DataRow row in dt.Rows)
+                {
+                    string phone = Convert.ToString(row["phone"]);
+                    readText[i] = phone;
+                    //MessageBox.Show(phone);
+                    SqlQuery("UPDATE " + base_name + " SET state=1 WHERE phone='" + phone + "'", "");
+                    i++;
+                }
+
+                return readText;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1_Click_1(null, null);
+            Random rnd = new Random();
+            int value = rnd.Next(100000, 999999);
+            textBox2.Text = value.ToString();
+
+            DateTime dt1 = DateTime.Now;
+            DateTime dt2 = DateTime.Now.AddDays(7);
+
+            textBox9.Text = dt1.ToString("dd-MM-yyyy");
+            textBox10.Text = dt2.ToString("dd-MM-yyyy");
+        }
+        
+        private void MakeBat(string login, string pass,bool pause)
+        {
+            textBox_batnik.Text += "net user " + login + " " + pass + " /add\r\n";
+            textBox_batnik.Text += "net localgroup \"Пользователи удаленного рабочего стола\" " + login + " /add\r\n";
+
+            if (pause==true)
+             textBox_batnik.Text += "pause\r\n";
+
+            File.WriteAllText("makeuser.bat", textBox_batnik.Text, Encoding.GetEncoding(866));
+        }
+               
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            db.Connect();
+            dataGridView1.DataSource = db.Get_DataTable("SELECT * from tab_clients");
+
+            try
+            {
+                string s = dataGridView1.Rows[dataGridView1.Rows.Count - 2].Cells[2].Value.ToString();
+                string t = s.Substring(s.Length - 2, 2);
+                textBox1.Text = "test05" + (Convert.ToInt32(t) + 1).ToString();
+                //MessageBox.Show(t);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            Random rnd = new Random();
+            int value = rnd.Next(100000, 999999);
+            textBox2.Text = value.ToString();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            string sql = "INSERT INTO `tab_clients` VALUES (NULL, NOW(),'" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "','" +
+            textBox5.Text + "','" + textBox6.Text + "','" + textBox7.Text + "','" + textBox8.Text + "','qwe','" + textBox9.Text + "','" + textBox10.Text + "','0','tnt','tnt','500');";
+            textBox_sql.Text = sql;
+
+            MessageBox.Show(sql);
+            db.SqlQuery(sql, "");
+
+            MakeBat(textBox1.Text, textBox2.Text, true);
+            System.Diagnostics.Process.Start("makeuser.bat");
+            button1_Click_1(null, null);
+        }
+
+        private void button3_Click_2(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+            {
+                string logn = dataGridView1.Rows[i].Cells[2].Value.ToString();
+                string pswd = dataGridView1.Rows[i].Cells[3].Value.ToString();
+
+                if (logn.IndexOf(",") > 0)
+                {
+                    string[] strArr = logn.Split(',');
+                    for (int j = 0; j < strArr.Length; j++)
+                    {
+                        //MessageBox.Show(strArr[j]);
+                        MakeBat(strArr[j], pswd, false);
+                    }
+                }
+                else
+                {
+                    MakeBat(logn, pswd, false);
+                    //MessageBox.Show(logn + ":" + pswd);
+                }                
+
+            }
+        }
+    }
+}
